4fb505c384e0cd0044d27bf1b430f67121f373ce
Laian <61033391+laianbraum@users.noreply.github.com>
2021-02-20 22:33:06 -0300

    :sparkles: Implementando sistema de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Header from './components/Header';
+import Home from './pages/Home';
+import CharacterPage from './pages/CharacterPage';
+
 function App() {
   return (
-    <div>
-      <h1>Hello world!</h1>
-    </div>
+    <BrowserRouter>
+      <Header/>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/character/:id" component={CharacterPage} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
