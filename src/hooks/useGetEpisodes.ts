78a764ed55e797df4ebb75376a1723b56cc1191c
Laian <61033391+laianbraum@users.noreply.github.com>
2021-02-21 00:23:38 -0300

    :recycle: feat(Episódios que participou) refatorada

--- src/hooks/useGetEpisodes.ts
+++ src/hooks/useGetEpisodes.ts
@@ -7,14 +7,8 @@ export default function useGetOneCharacter(character : string | undefined)
   const [ episodes, setEpisodes ] = useState<Episode[]>();
 
   useEffect(() => {
-    api.get(`/episodes/`).then((response) => {
-      let episodesAux: Episode[] = [];
-      response.data.forEach((episode : Episode) => {
-        if (character && episode.characters.includes(character)){
-          episodesAux = [...episodesAux, episode]
-        }
-      })
-      setEpisodes(episodesAux);
+    api.get(`/episodes?characters=${character}`).then((response) => {
+      setEpisodes(response.data);
     });
   }, [character]);
 
