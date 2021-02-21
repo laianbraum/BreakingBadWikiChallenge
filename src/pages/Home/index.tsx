e35dd6d0a3c53b04e593d0cb38898de014859bb2
Laian <61033391+laianbraum@users.noreply.github.com>
2021-02-20 22:59:33 -0300

    :sparkles: Implementando página Home c/ todos personagens

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import useGetAllCharacters from '../../hooks/useGetAllCharacters';
+
+import { CardsContainer } from '../../styles/CharacterCard';
+import CharacterCard from '../../components/CharacterCard';
+
+export default function HomePage(){
+
+  const characters = useGetAllCharacters(); 
+
+  if(!characters){
+    return <h1>Loading...</h1>
+  }
+
+  return (
+    <CardsContainer>
+      {
+        characters.map((character) => {
+          return (
+            <Link
+              to={`/character/${character.char_id}`}
+              key={character.char_id}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <CharacterCard
+                key={character.name}
+                character={character}
+              />
+            </Link>
+          )
+        })
+      }
+    </CardsContainer>
+  )
+}
